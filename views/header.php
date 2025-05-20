c6760691de230d25c09bb8bac09a2f6bd1d5a0e7
niyobern <niyobern@outlook.com>
2025-05-20 12:50:59 +0000

    Remove surveys link from header: Clean up navigation by deleting the surveys link from the header view, streamlining the user interface.

--- views/header.php
+++ views/header.php
@@ -23,9 +23,6 @@
                         <li class="nav-item">
                             <a class="nav-link" href="index.php?page=projects">Research Projects</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?page=surveys">Surveys</a>
-                        </li>
                     <?php endif; ?>
                 </ul>
                 <ul class="navbar-nav">
